b0ab24a1b983b59b8cff37c98b270bcd7f0e0329
Miguel Mendoza <miguel@lisainsurtech.com>
2022-10-28 10:56:43 -0400

    Create form and list component. Add actions and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // Ngxs modules (devs included)
 import {NgxsModule} from '@ngxs/store';
@@ -9,12 +8,20 @@ import {NgxsReduxDevtoolsPluginModule} from '@ngxs/devtools-plugin';
 import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+// Components
+import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { CreateFormComponent } from './components/create-form/create-form.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CreateFormComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     NgxsModule.forRoot(),
     NgxsReduxDevtoolsPluginModule.forRoot(),
     NgxsLoggerPluginModule.forRoot(),
